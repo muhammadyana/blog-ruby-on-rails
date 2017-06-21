a79611ad28adcbf8d5eaf1c974e0ded68a96d987
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2017-06-21 10:11:25 +0700

    user association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
 	#attr_accessor :slug
+	belongs_to :user
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 	after_create :send_email
 	validates :title, :description, :image, presence: true
 	validates :title, uniqueness: true
+	validates :user_id, presence: true
 	mount_uploader :image, ArticlesUploader
 	private
 		def send_email
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621020403) do
+ActiveRecord::Schema.define(version: 20170621023824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170621020403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "user_id"
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
